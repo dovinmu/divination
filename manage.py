a410653c461aaf7f78c804825976bf8f256947e0
Rowan Copley <rowan.copley@gmail.com>
2020-05-07 12:56:52 -0700

    import datetime

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@
 
 if __name__ == "__main__":
     import astrology
-    from ephem import city
     astrology.now_cast()
 
+    from ephem import city
+    from datetime import datetime
     test_birth = city('Washington')
     astrology.horoscope('Human Bean', test_birth, datetime(2000, 1, 1, 1, 1), 'US/Eastern')
