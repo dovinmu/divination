e3499e861f81db9f984e61525904e2270a5a3bd2
Rowan Copley <rowan.copley@gmail.com>
2021-02-05 11:53:32 -0800

    rearrange files

---
+++ divination/chinese.py
@@ -0,0 +1,14 @@
+signs = {
+    'rat':['yang','north','Mid-Winter','water',	1],
+    'ox':['yin','north','Late Winter','earth',	2],
+    'tiger':['yang', 'east', 'Early Spring', 'wood', 3],
+    'rabbit':['yin', 'east', 'Mid-Spring', 'wood', 4],
+    'dragon':['yang', 'east', 'Late Spring', 'earth', 1],
+    'snake': ['yin', 'south', 'Early Summer', 'fire', 2],
+    'horse': ['yang', 'south', 'Mid-Summer', 'fire', 3],
+    'goat':	['yin',	'south', 'Late Summer', 'earth', 4],
+    'monkey': ['yang', 'west', 'Early Autumn', 'metal', 1],
+    'rooster': ['yin', 'west', 'Mid-Autumn', 'metal', 2],
+    'dog': ['yang', 'west', 'Late Autumn', 'earth', 3],
+    'pig': ['yin', 'north', 'Early Winter', 'water', 4]
+}
