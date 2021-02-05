7df94a5b3200bbc9af8a4fc48754f44c930ee300
Rowan Copley <rowan.copley@gmail.com>
2021-02-05 13:42:59 -0800

    test for case where we just pass in a city string

--- manage.py
+++ manage.py
@@ -8,3 +8,9 @@ if __name__ == "__main__":
     from datetime import datetime
     test_birth = city('Washington')
     astrology.horoscope('Human Bean', test_birth, datetime(2000, 1, 1, 1, 1), 'US/Eastern')
+
+    name = "Roy G. Biv"
+    birth_city = "Washington"
+    birth_dt = datetime(1070, 1, 1, 1, 1)
+    birth_tz = "US/Eastern"
+    horoscope = astrology.horoscope(name, birth_city, birth_dt, birth_tz)
