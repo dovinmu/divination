0220180c561b09cf058ed48ce5b5c57dad985e32
Rowan Copley <dovinmu@gmail.com>
2021-02-05 11:22:33 -0800

    rearrange for setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='divination',
+      version='0.1',
+      description='utilities for divination',
+      author='Z. Rowan Copley',
+      url='https://github.com/dovinmu/divination',
+      packages=['divination'],
+     )
